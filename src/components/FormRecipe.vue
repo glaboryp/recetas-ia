1c80cf0e98a84464c7a4d936fdc9ef2c7ecd6e39
Gloria Labory <glabory@gmail.com>
2024-07-11 18:17:52 +0200

    feat: title and form to fill out the recipe

---
+++ src/components/FormRecipe.vue
@@ -0,0 +1,66 @@
+<template>
+  <form>
+    <div class="form-question">
+      <Badge value="1" size="large" severity="primary"></Badge>
+      <p>¿Qué ingredientes quieres usar?</p>
+    </div>
+    <Textarea v-model="ingredientes" rows="1" />
+
+    <div class="form-question">
+      <Badge value="2" size="large" severity="primary"></Badge>
+      <p>¿Para cuántas personas estás cocinando?</p>
+    </div>
+    <InputNumber v-model="persons" fluid showButtons />
+
+    <div class="form-question">
+      <Badge value="3" size="large" severity="primary"></Badge>
+      <p>¿Cuánto tiempo tienes?</p>
+    </div>
+    <div class="form-time">
+      <span>{{ time }} minutos</span>
+      <Slider v-model="time" :step="10" :min="5" :max="120" class="form-slider" />
+    </div>
+
+    <Button label="¡Oído cocina!" />
+  </form>
+
+  <ProgressBar :value="progress" v-if="progress < 100"></ProgressBar>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const ingredientes = ref('')
+const persons = ref(1)
+const time = ref(5)
+const progress = ref(100)
+</script>
+
+<style scoped>
+form {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 80%;
+}
+
+.form-question {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 1.2rem;
+}
+
+.form-time {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.form-slider {
+  width: 90%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>¿Tienes poco tiempo y <span class="title-color">quieres comer bien?</span></h1>
+    <h1>
+      ¿Quieres sorprender a los tuyos con una buena comida pero
+      <span class="title-color">no sabes cómo hacerlo?</span>
+    </h1>
+    <h2>
+      Di adiós a las comidas aburridas, con recomendaciones de recetas impulsadas por inteligencia
+      artificial a partir de los ingredientes que tú decidas.
+    </h2>
+
+    <FormRecipe v-if="status === 0" @changeStatus="(n) => (status = n)" />
+    <ResultRecipe v-if="status === 1" />
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import FormRecipe from '@/components/FormRecipe.vue'
+import ResultRecipe from '@/components/ResultRecipe.vue'
+
+const status = ref(0)
+</script>
+
+<style scoped>
+main {
+  padding: 20px 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  color: white;
+  font-size: 3.5rem;
+  line-height: 1.2;
+  font-weight: bolder;
+}
+
+h2 {
+  margin-top: 20px;
+  font-size: 1.7rem;
+  line-height: 1.5;
+}
+
+.title-color {
+  color: #facc15;
+  font-weight: bolder;
+}
+</style>
