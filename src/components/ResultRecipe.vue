ff2fd215afe18cd5ce2f4adcbddc7b333bd24fdf
Gloria Labory <glabory@gmail.com>
2024-07-12 20:50:02 +0200

    feat: view for a recipe created with ia

--- src/components/ResultRecipe.vue
+++ src/components/ResultRecipe.vue
@@ -1,7 +1,32 @@
 <template>
-  <h1></h1>
+  <h2>Receta</h2>
+  <div v-html="output"></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { marked } from 'marked'
+import { computed } from 'vue'
 
-<style scoped></style>
+const props = defineProps(['recipe'])
+
+const output = computed(() => marked(props.recipe))
+</script>
+
+<style scoped>
+h2 {
+  color: #facc15;
+  font-size: 2.5rem;
+  margin-top: 20px;
+}
+div {
+  border: 1px solid #ffffff1f;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+@media (width < 500px) {
+  h2 {
+    font-size: 1.2rem;
+  }
+}
+</style>
