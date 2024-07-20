342434f7d6eb8729e754a5631cdf4583db884f31
Gloria Labory <glabory@gmail.com>
2024-07-20 19:27:26 +0200

    test: first test

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,8 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
+describe('Visit home page', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('h1', '¿Tienes poco tiempo')
   })
 })
