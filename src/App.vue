141fc188b5534e496c00d763eef403d0c4197665
Gloria Labory <glabory@gmail.com>
2024-07-20 19:25:06 +0200

    style: change in composition styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup></script>
-
 <template>
-  <RouterView />
+  <MenuBar />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
+<script setup>
+import MenuBar from './components/MenuBar.vue'
+</script>
+
 <style>
-body {
+main {
+  padding: 20px 100px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 1300px;
+  height: 100%;
+  align-self: center;
+  width: 100%;
+}
+
+@media (width < 900px) {
+  main {
+    padding: 20px 50px;
+  }
+}
+
+@media (width < 750px) {
+  main {
+    padding: 20px 30px;
+  }
+}
+
+@media (width < 500px) {
+  main {
+    padding: 20px 10px;
+  }
 }
 </style>
