134ed0290f1de7b07589d101b145539ef067e103
Gloria <glabory@gmail.com>
2024-07-31 19:53:51 +0200

    fix: Improve prompt ai

--- src/composables/ai.js
+++ src/composables/ai.js
@@ -7,6 +7,7 @@ const google = createGoogleGenerativeAI({
 
 export async function useCreateRecipe(ingredients, persons, time, lunch) {
   const prompt = `Dame una receta para ${lunch} con los siguientes ingredientes ${ingredients} (y solo estos ingredientes) y que sea fácil de hacer.
+  Lo primero que me tienes que decir es el nombre de la receta.
   La receta debe contener los ingredientes que he mencionado anteriormente (son estos: ${ingredients}) y no puede contener ningún otro ingrediente, a no ser que sea un ingrediente básico como sal, aceite, agua, etc.
   La receta debe ser para ${persons} personas, por lo que tienes que decir la cantidad de ingredientes necesarios para ese número de personas.
   La receta no debe tardar más de ${time} minutos en prepararse.
