6e27578eb8f9b1d41be310d0fbff9f0a4010cd1a
Gloria Labory <glabory@gmail.com>
2024-08-01 17:48:19 +0200

    feat: empty state for user recipes view

--- src/views/UserRecipesView.vue
+++ src/views/UserRecipesView.vue
@@ -1,44 +1,62 @@
 <template>
-  <h1 class="title" v-if="recipes.length === 0">No tiene aún recetas guardadas</h1>
-  <div v-else class="container">
-    <h1 class="title">Recetas guardadas</h1>
+  <div class="container">
+    <h1 class="title" v-if="Object.keys(recipes).length === 0 && !loading">
+      No tiene aún recetas guardadas
+    </h1>
+    <h1 class="title" v-else>Recetas guardadas</h1>
     <div class="recipes-list">
       <template v-for="(recipe, index) in recipes" :key="recipe.key">
-        <RecipeCard 
-          :id="index" 
-          :recipe="recipe" 
+        <RecipeCard
+          :id="index"
+          :recipe="recipe"
           :favorite="recipe.favorite"
           @changeFavorite="(n) => (recipe.favorite = n)"
           @changeId="(n) => (index = n)"
         />
       </template>
     </div>
+    <div class="loading" v-if="loading">
+      <p class="loading-text">¡Recuperando las recetas!</p>
+      <ProgressBar mode="indeterminate"></ProgressBar>
+    </div>
   </div>
+
+  <Toast />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { getDatabase, ref as refFirebase, child, get } from 'firebase/database'
 import { useAuthStore } from '@/stores/authStore'
+import { useToast } from 'primevue/usetoast'
 
 const authStore = useAuthStore()
 const dbRef = refFirebase(getDatabase())
+const toast = useToast()
 
 const recipes = ref({})
+const loading = ref(true)
 
 get(child(dbRef, `user-recipes/${authStore.userId}`))
-.then((snapshot) => {
-  if (snapshot.exists()) {
-    recipes.value = snapshot.val()
-    Object.values(recipes.value).forEach(recipe => {
-      recipe['favorite'] = true
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      recipes.value = snapshot.val()
+      Object.values(recipes.value).forEach((recipe) => {
+        recipe['favorite'] = true
+      })
+    } else {
+      recipes.value = {}
+    }
+    loading.value = false
+  })
+  .catch(() => {
+    toast.add({
+      severity: 'error',
+      summary: 'Info',
+      detail: 'Ha ocurrido un error al recuperar sus recetas guardadas, inténtelo en unos minutos',
+      life: 3000
     })
-  } else {
-    recipes.value = {}
-  }
-}).catch((error) => {
-  console.error(error)
-})
+  })
 </script>
 
 <style scoped>
@@ -61,6 +79,19 @@ get(child(dbRef, `user-recipes/${authStore.userId}`))
   width: 70%;
   grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
   gap: 20px;
+}
+
+.loading {
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  height: 70dvh;
+  justify-content: center;
+}
+
+.loading-text {
+  text-align: center;
+  font-size: 1.5rem;
 }
 
 @media (width < 700px) {
