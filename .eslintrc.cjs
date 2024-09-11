6242d40a6e404145471807b17311c43fc673b8a4
Gloria Labory <glabory@gmail.com>
2024-09-11 23:03:59 +0200

    config: change eslint version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,31 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  ignorePatterns: fs
+    .readFileSync(path.resolve(__dirname, '.gitignore'), 'utf8')
+    .split('\n')
+    .filter(Boolean),
   overrides: [
     {
       files: [
+        '*.vue',
+        '*.js',
+        '*.jsx',
+        '*.cjs',
+        '*.mjs',
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
         'cypress/support/**/*.{js,ts,jsx,tsx}'
       ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
